51964a5b37c9b5fe15bdfe3a057568bb152d15de
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-05-19 11:11:01 -0300

    Add list the total balance inside the cards and formatValue to 'REAL'

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,8 @@
+// Formatar o 'formato' do valor, numéro em moeda Real
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
 
 export default formatValue;
